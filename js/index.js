8d0338433336b5dd0497a31d8a1193a6a678c7cb
Joe McDonough <joemcdonoughmusic.com>
2022-04-09 21:41:24 -0400

    first commit, built to the given designs, mobile first, responsive, and incorporating javascript

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+const submitButton = document.getElementById('rating-submit-button');
+const ratingCard = document.getElementById('rating');
+const thankYouCard = document.getElementById('thank-you');  
+const radios = document.querySelectorAll('input[type=radio]');
+const thankYouSmallHeader = document.getElementById('thank-you-small-header');
+
+
+radios.forEach((radio) => {
+    radio.addEventListener('click', (e) => {
+        if (e.target.checked) {
+            thankYouSmallHeader.innerText = `You Selected ${e.target.nextElementSibling.innerText
+} out of 5`;
+        }
+    });
+});
+
+submitButton.addEventListener('click', () => {
+    ratingCard.classList.add('hide');
+    thankYouCard.classList.remove('hide');
+    });
+
+
+
+
+
+
