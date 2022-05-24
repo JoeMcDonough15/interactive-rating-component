cf5e2cf176de93cbf1d7867e6cf333cb675082be
Joe McDonough <joemcdonoughmusic.com>
2022-05-23 20:00:58 -0400

    fix Accessibility issue by adding level one header, remove empty header on thank you state, and remove type attribute from script tag

--- js/index.js
+++ js/index.js
@@ -1,28 +1,21 @@
-
-const submitButton = document.getElementById('rating-submit-button');
-const ratingCard = document.getElementById('rating');
-const thankYouCard = document.getElementById('thank-you');  
-const radios = document.querySelectorAll('input[type=radio]');
-const thankYouSmallHeader = document.getElementById('thank-you-small-header');
-
-
+const submitButton = document.getElementById("rating-submit-button");
+const ratingCard = document.getElementById("rating");
+const thankYouCard = document.getElementById("thank-you");
+const radios = document.querySelectorAll("input[type=radio]");
+const thankYouSmallHeader = document.createElement("h5");
+const thankYouImg = document.getElementById("thank-you-img");
 radios.forEach((radio) => {
-    radio.addEventListener('click', (e) => {
-        if (e.target.checked) {
-            thankYouSmallHeader.innerText = `You Selected ${e.target.nextElementSibling.innerText
-} out of 5`;
-        }
-    });
+  radio.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      thankYouSmallHeader.innerText = `You Selected ${e.target.nextElementSibling.innerText} out of 5`;
+    }
+  });
 });
 
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    ratingCard.classList.add('hide');
-    thankYouCard.classList.remove('hide');
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  ratingCard.classList.add("hide");
+  thankYouCard.classList.remove("hide");
+  thankYouSmallHeader.classList.add("thank-you-small-header");
+  thankYouImg.after(thankYouSmallHeader);
 });
-
-
-
-
-
-
